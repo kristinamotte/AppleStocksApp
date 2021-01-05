84a5f602980bae6d473e440817861a6a2aa33640
Kristina <mottechristina@gmail.com>
2021-01-05 22:25:42 +0200

    create new project

---
+++ AppleStocksApp/AppleStocksApp/AppleStocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleStocksAppApp.swift
+//  AppleStocksApp
+//
+//  Created by Kristina Motte on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct AppleStocksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleStocksApp/AppleStocksApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleStocksApp
+//
+//  Created by Kristina Motte on 1/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
