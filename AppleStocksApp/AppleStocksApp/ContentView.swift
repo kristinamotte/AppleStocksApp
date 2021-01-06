68770dd68c11d14d2b51c8fdabad21190d32d8a3
Kristina <mottechristina@gmail.com>
2021-01-06 20:33:30 +0200

    add date and search

--- AppleStocksApp/AppleStocksApp/ContentView.swift
+++ AppleStocksApp/AppleStocksApp/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchTearm: String = ""
+    
+    init() {
+        UINavigationBar.appearance().backgroundColor = .black
+        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        UITableView.appearance().backgroundColor = .black
+        UITableViewCell.appearance().backgroundColor = .black
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView() {
+            ZStack(alignment: .leading) {
+                Color.black.edgesIgnoringSafeArea(.all)
+                VStack(alignment: .leading) {
+                    Text("January 5")
+                        .foregroundColor(.gray)
+                        .font(.custom("Arial", size: 32))
+                        .fontWeight(.bold)
+                        .padding(EdgeInsets(top: 8, leading: 16, bottom: 0, trailing: 0))
+                    
+                    SearchView(searchItem: $searchTearm)
+                        .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+                    Spacer()
+                }
+            }
+            
+            .navigationBarTitle("Stocks")
+        }
     }
 }
 
